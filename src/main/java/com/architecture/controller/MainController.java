3105ce9c5f1c018161acd82f11d39e1f8d96d11d
pineconetechs <60033130+pineconetechs@users.noreply.github.com>
2020-01-20 11:21:02 +0300

    master

---
+++ src/main/java/com/architecture/controller/MainController.java
@@ -0,0 +1,83 @@
+package com.architecture.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+
+@Controller
+    public class MainController {
+    @RequestMapping("/")
+    public ModelAndView page() {
+
+        ModelAndView modelAndView = new ModelAndView("index");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/about.html")
+    public ModelAndView about() {
+
+        ModelAndView modelAndView = new ModelAndView("about");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/portfolio.html")
+    public ModelAndView port() {
+
+        ModelAndView modelAndView = new ModelAndView("portfolio");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/contact.html")
+    public ModelAndView contact() {
+
+        ModelAndView modelAndView = new ModelAndView("contact");
+
+        return modelAndView;
+    }
+
+    @Autowired
+    public JavaMailSender javaMailSender;
+
+
+    @PostMapping("/")
+    public String sendMail(@RequestParam("name") String name, @RequestParam("subject") String subject, @RequestParam("email") String email, @RequestParam("phone") String phone, @RequestParam("text") String text)
+            throws MessagingException, IOException {
+
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+        helper.setTo("eyyup.guzel@ozu.edu.tr");
+
+
+        helper.setSubject("Subject");
+
+
+        // true = text/html
+        helper.setText(name + "\n" + email + "\n" + phone + "\n" + text, false);
+
+
+        javaMailSender.send(msg);
+
+
+        return "index";
+
+
+    }
+
+}
+
